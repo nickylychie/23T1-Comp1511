b4c876bd292f57c0e7da799440bfb1893942d122
Nicole <z5206727@unsw.edu.au>
2023-02-13 20:32:05 +1100

    Added tutorial demo code

---
+++ week_1/hello_world.c
@@ -0,0 +1,45 @@
+// Basic Hello World Program
+// Nicole, 23T1 Week 1
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello World\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   /\___/
+//  (=' w '=)
